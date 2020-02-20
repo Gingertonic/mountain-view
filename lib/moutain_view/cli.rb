c93ec99edf6e04b4979efcbee7578a667d33663b
Gingertonic <bethmschofield@gmail.com>
2020-02-20 17:42:43 +0000

    stub list_mountains method in cli

--- lib/moutain_view/cli.rb
+++ lib/moutain_view/cli.rb
@@ -4,6 +4,11 @@ class MoutainView::CLI
         puts "Welcome to the Mountain Range!"
         farewell
     end
+
+    def list_mountains 
+        puts "Here is a list of mountains..."
+
+    end
     
     def farewell 
         puts "bye!"
